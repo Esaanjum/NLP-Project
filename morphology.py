78eb366bd1b7b4b67527b524d383861352cb2bf8
Esaanjum <esaanjum14@gmail.com>
2023-02-02 12:37:36 +0500

    pdf file read with python

---
+++ morphology.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 12:30:40 2023
+
+@author: Esa
+"""
+
---
+++ preprocessing.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 17 10:02:18 2023
+
+@author: Esa
+"""
+import spacy
+import PyPDF2
+
+import os
+
+# spacy english model (large)
+nlp = spacy.load('en_core_web_lg')
+
+# method for reading a pdf file
+def readPdfFile(filename, folder_name):
+    
+    # storing path of PDF-Documents folder
+    data_path = str(os.getcwd()) + "\\" + folder_name
+
+    file = open(data_path + "\\" + filename, mode="rb")
+
+    # looping through pdf pages and storing data
+    pdf_reader = PyPDF2.PdfReader(file)
+    num_pages = len(pdf_reader.pages)
+
+    # traverse through each page and store data as an element in list
+    text = []
+    for page_num in range(0, num_pages):
+        current_page = pdf_reader.pages[page_num]
+        text.append(current_page.extract_text().replace("\n","").lower())
+        
+    return text
+
